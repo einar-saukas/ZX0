a29ad10f4e4d01553d5768f55d37a39ed12f4bec
einar-saukas <einar@summa.com.br>
2021-02-07 19:39:38 -0300

    fixed compression with prefix

--- src/dzx0.c
+++ src/dzx0.c
@@ -157,7 +157,7 @@ int main(int argc, char *argv[]) {
     int forced_mode = FALSE;
     int i;
 
-    printf("DZX0 v1.4: Data decompressor by Einar Saukas\n");
+    printf("DZX0 v1.5: Data decompressor by Einar Saukas\n");
 
     /* process hidden optional parameters */
     for (i = 1; i < argc && *argv[i] == '-'; i++) {
